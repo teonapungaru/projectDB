78950c41a05f287caff3e6f685620b02af537037
teonapungaru <teonapungaru.97@gmail.com>
2019-05-13 15:56:41 +0300

    added Service class

--- src/main/java/com/project/db/projectDB/model/Service.java
+++ src/main/java/com/project/db/projectDB/model/Service.java
@@ -1,4 +1,26 @@
 package com.project.db.projectDB.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Instant entryDate;
 }
